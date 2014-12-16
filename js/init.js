34fdface78381fd7e340d9224c2797f26398009a
mak7sus <mak7sus@gmail.com>
2014-12-16 20:25:40 +0200

    no message

--- js/init.js
+++ js/init.js
@@ -1,3 +1,7 @@
-head.load("js/lib/jquery.js", 
+head.load("js/lib/jquery.js",
+					"js/lib/jquery.min.js",
 					"js/lib/cycle.js",
+					"js/lib/tinyscrollbar.js",					
+					"js/lib/checkable.js",
+					"js/lib/jquery-ui.custom.min.js",
 					"js/common.js");
